766ddd4adc7003d0da20946e8658c91e3cbc793d
aayush <aayush.malviya@psquarecompany.com>
2026-06-10 17:59:49 +0530

    chnages

--- App.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { BusinessCardScannerPage } from "./pages/BusinessCardScanner";
-export default function App() {
-    return (_jsx(BrowserRouter, { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(BusinessCardScannerPage, {}) }), _jsx(Route, { path: "*", element: _jsx(Navigate, { to: "/", replace: true }) })] }) }));
-}
